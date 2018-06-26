c8f36f9350f5901481677b352373191d2b32d1e7
Taewon Kang <itschool@itsc.kr>
2018-06-26 21:35:40 +0900

    (18.06.12) DFS를 활용한 최단경로-연구활동 가는길

---
+++ ch08/연구활동 가는 길.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAX 100
+#define INF 987654321
+
+int n, m, min, G[MAX][MAX], visited[MAX];
+
+void dfs(int k, int dist) {
+    visited[k] = 1;
+    if(k == n) {
+        if(min > dist) min = dist;
+        return;
+    }
+    for(int i=1; i<=n; i++) {
+        if(!visited[i] && G[k][i]) {
+            dfs(i, dist+G[k][i]);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main() {
+    int a, b, w;
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < m; i++) {
+        scanf("%d %d %d", &a, &b, &w);
+        G[a][b] = G[b][a] = w;
+    }
+    min = INF;
+    dfs(1,0);
+    printf("%d", min);
+    return 0;
+}
